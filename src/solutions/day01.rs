8300d1aa4fa86a072dd4e97a632d9537cc687bd9
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-12-01 09:41:37 +0100

    increase readability

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,22 +1,33 @@
 use crate::utils::{date::Date, io::read_input};
 
-fn get_calorie_sums(input: &Vec<String>) -> Vec<i32> {
+fn get_calorie_sums(input: &[String]) -> Vec<i32> {
     let parsed: Vec<Vec<i32>> = input
-        .split(|item| item.is_empty())
-        .map(|group| group.iter().map(|s| s.parse().unwrap()).collect())
+        .split(|it| it.is_empty())
+        .map(|group| {
+            group
+                .iter()
+                .map(|it| it.parse().expect("Failed to parse calorie count"))
+                .collect()
+        })
         .collect();
 
-    parsed.iter().map(|group| group.iter().sum()).collect()
+    let sums = parsed.iter().map(|group| group.iter().sum()).collect();
+
+    sums
 }
 
-fn part1(input: &Vec<String>) -> i32 {
+fn part1(input: &[String]) -> i32 {
     let calorie_sums = get_calorie_sums(input);
-    let max_calories = calorie_sums.iter().max().unwrap().to_owned();
+    let max_calories = calorie_sums
+        .iter()
+        .max()
+        .expect("Failed to calculate max calories")
+        .to_owned();
 
     max_calories
 }
 
-fn part2(input: &Vec<String>) -> i32 {
+fn part2(input: &[String]) -> i32 {
     let mut calorie_sums = get_calorie_sums(input);
 
     // Sort in descending order
@@ -24,7 +35,9 @@ fn part2(input: &Vec<String>) -> i32 {
 
     let top_three = calorie_sums.iter().take(3).copied().collect::<Vec<i32>>();
 
-    top_three.iter().sum()
+    let sum_of_top_three = top_three.iter().sum();
+
+    sum_of_top_three
 }
 
 pub fn solve() {
