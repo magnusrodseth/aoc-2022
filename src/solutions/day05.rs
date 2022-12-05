ee6da092d893318ab2ebaee261b3df9266254813
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-12-05 14:12:44 +0100

    finish day 5 with readability improvements

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -30,13 +30,12 @@ impl Instruction {
 #[derive(Clone)]
 struct Crates<const STACKS_COUNT: usize> {
     stacks: [Vec<char>; STACKS_COUNT],
-    instructions: Vec<Instruction>,
 }
 
 impl<const STACKS_COUNT: usize> Crates<STACKS_COUNT> {
     /// Creates new crates from a string of characters.
     /// The string is split into `STACKS_COUNT` stacks.
-    fn new(input: &str, instructions: Vec<Instruction>) -> Self {
+    fn new(input: &str) -> Self {
         let mut stacks = from_fn(|_| Vec::new());
 
         for line in input.lines().rev() {
@@ -49,62 +48,76 @@ impl<const STACKS_COUNT: usize> Crates<STACKS_COUNT> {
             }
         }
 
-        Self {
-            stacks,
-            instructions,
-        }
+        Self { stacks }
     }
 
+    /// Pops off the top crate from each stack in the collection of crates.
     fn top_crates(&mut self) -> String {
         self.stacks
             .iter_mut()
             .map(|stack| stack.pop().expect("No crates left"))
             .collect()
     }
+
+    fn move_one(&mut self, from: usize, to: usize) {
+        let value = self.stacks[from].pop().expect("No crates left");
+        self.stacks[to].push(value);
+    }
+
+    fn move_many(&mut self, from: usize, to: usize, quantity: usize) {
+        let mut crates_to_move = self.stacks[from].split_off(self.stacks[from].len() - quantity);
+        self.stacks[to].append(&mut crates_to_move);
+    }
 }
 
-/// Parsing input is the biggest hassle in this solution.
-/// First, we must separate the stacks from the instructions.
-fn parse_input<const STACKS_COUNT: usize>(input: &str) -> Crates<STACKS_COUNT> {
+struct CrateContainer<const STACKS_COUNT: usize> {
+    crates: Crates<STACKS_COUNT>,
+    instructions: Vec<Instruction>,
+}
+
+fn parse_input<const STACKS_COUNT: usize>(input: &str) -> CrateContainer<STACKS_COUNT> {
     let (crates_input, instruction_input) = input.split_once("\n\n").expect("Invalid input");
     let instructions = Instruction::parse_instructions(instruction_input);
+    let crates = Crates::new(crates_input);
 
-    Crates::new(crates_input, instructions)
+    CrateContainer {
+        crates,
+        instructions,
+    }
 }
 
-fn part1<const STACKS_COUNT: usize>(crates: &Crates<STACKS_COUNT>) -> String {
+fn part1<const STACKS_COUNT: usize>(container: &CrateContainer<STACKS_COUNT>) -> String {
     // We clone the borrowed `crates` to avoid borrowing it mutably.
     // This way, part1 and part2 can be called on the same crates.
-    let mut crates = crates.clone();
+    let mut crates = container.crates.clone();
+    let instructions = &container.instructions;
 
-    for instruction in &crates.instructions {
+    for instruction in instructions {
         for _ in 0..instruction.quantity {
-            let value = crates.stacks[instruction.from as usize - 1]
-                .pop()
-                .expect("No crates left");
+            let from = instruction.from as usize - 1;
+            let to = instruction.to as usize - 1;
 
-            crates.stacks[instruction.to as usize - 1].push(value);
+            crates.move_one(from, to);
         }
     }
 
     crates.top_crates()
 }
 
-fn part2<const STACKS_COUNT: usize>(crates: &Crates<STACKS_COUNT>) -> String {
+fn part2<const STACKS_COUNT: usize>(container: &CrateContainer<STACKS_COUNT>) -> String {
     // We clone the borrowed `crates` to avoid borrowing it mutably.
     // This way, part1 and part2 can be called on the same crates.
-    let mut crates = crates.clone();
+    let mut crates = container.crates.clone();
+    let instructions = &container.instructions;
 
-    for instruction in &crates.instructions {
+    for instruction in instructions {
         let (quantity, from, to) = (
             instruction.quantity as usize,
             instruction.from as usize - 1,
             instruction.to as usize - 1,
         );
 
-        let mut crates_to_move =
-            crates.stacks[from].split_off(crates.stacks[from].len() - quantity);
-        crates.stacks[to].append(&mut crates_to_move);
+        crates.move_many(from, to, quantity);
     }
 
     crates.top_crates()
@@ -112,10 +125,10 @@ fn part2<const STACKS_COUNT: usize>(crates: &Crates<STACKS_COUNT>) -> String {
 
 pub fn solve() {
     let input = include_str!("../../input/day05.txt");
-    let crates = parse_input::<9>(input);
+    let container = parse_input::<9>(input);
 
-    println!("Day 1 Part 1: {:?}", part1::<9>(&crates));
-    println!("Day 1 Part 2: {:?}", part2::<9>(&crates));
+    println!("Day 1 Part 1: {:?}", part1::<9>(&container));
+    println!("Day 1 Part 2: {:?}", part2::<9>(&container));
 }
 
 #[cfg(test)]
@@ -134,13 +147,13 @@ move 1 from 1 to 2";
 
     #[test]
     fn day05_part1() {
-        let crates = parse_input::<3>(INPUT);
-        assert_eq!(part1(&crates), "CMZ");
+        let container = parse_input::<3>(INPUT);
+        assert_eq!(part1(&container), "CMZ");
     }
 
     #[test]
     fn day05_part2() {
-        let crates = parse_input::<3>(INPUT);
-        assert_eq!(part2(&crates), "MCD");
+        let container = parse_input::<3>(INPUT);
+        assert_eq!(part2(&container), "MCD");
     }
 }
